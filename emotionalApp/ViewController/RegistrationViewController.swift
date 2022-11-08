735e32d7f830796b22342d52302f9c3d6936a735
Aleksandr Eliseev <9120603@gmail.com>
2022-11-08 17:15:25 +0200

    Добавлен экран регистрации с формой и записью данных в UserDefaults. Добавлена проверка на заполнение всех форм и совпадение форм с паролями. Добавлен переход на второй экран выбора целей по условию заполнения всех форм и совпадения форм с паролем. Добавлена проверка модели в JSON. Добавлен алерт при незаполнении всех форм или несовпадении паролей.

---
+++ emotionalApp/ViewController/RegistrationViewController.swift
@@ -0,0 +1,93 @@
+//
+//  RegistrationViewController.swift
+//  emotionalApp
+//
+//  Created by Aleksandr Eliseev on 08.11.2022.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    
+    private var userDefaults = UserDefaults.standard
+    private var userData: UserDataModel?
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    private enum Keys: String {
+        case userData
+    }
+    
+    @IBAction func registerActionButton(_ sender: UIButton) {
+        if !nameTextField.text!.isEmpty && !ageTextField.text!.isEmpty && !loginTextField.text!.isEmpty && !emailTextField.text!.isEmpty && !passwordTextField.text!.isEmpty && repeatPasswordTextField.text == passwordTextField.text {
+            store(name: nameTextField.text!, age: ageTextField.text!, login: loginTextField.text!, email: emailTextField.text!, password: passwordTextField.text!)
+            goToNextVC()
+            print("\(userCheck())")
+        } else {
+                showNoDataAlert()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func goToNextVC() {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "SecondScreenViewController") else { return }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func showNoDataAlert() {
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: "Заполните все поля",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default)
+
+        alert.addAction(action)
+        self.present(alert, animated: true)
+    }
+    
+    private func showWrongPasswordAlert() {
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: "Пароли не совпадают",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default)
+
+        alert.addAction(action)
+        self.present(alert, animated: true)
+    }
+    
+    private func store(name: String, age: String, login: String, email: String, password: String) {
+        let currentUser = UserDataModel(
+            name: nameTextField.text ?? "",
+            age: ageTextField.text ?? "",
+            login: loginTextField.text ?? "",
+            email: emailTextField.text ?? "",
+            password: passwordTextField.text ?? "")
+        
+        guard let data = try? JSONEncoder().encode(currentUser) else {
+            print("Невозможно собрать модель из currentUser")
+            return
+        }
+        
+        userDefaults.set(data, forKey: Keys.userData.rawValue)
+    }
+    
+    private func userCheck() -> UserDataModel {
+        guard let data = userDefaults.data(forKey: Keys.userData.rawValue), let record = try? JSONDecoder().decode(UserDataModel.self, from: data) else {
+            return .init(name: "NO", age: "NO", login: "NO", email: "NO", password: "NO")
+        }
+        return record
+    }
+
+}
